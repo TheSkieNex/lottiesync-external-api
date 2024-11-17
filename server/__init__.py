28cbfedbec7f430e225b942715117610c74a2b54
CCXLV <abracadabragio12@gmail.com>
2024-11-17 20:23:31 +0400

    updated version

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,15 @@
-import uvicorn
 import logging
+import uvicorn
+
 from fastapi import FastAPI
 
 from server.router import router
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("server.log", mode='a'), logging.StreamHandler()]
+)
 
 def create_app():
     app = FastAPI()
@@ -14,9 +21,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-
-    uvicorn.run(app, host='127.0.0.1', port=7262, log_level='info', access_log=False)
+    uvicorn.run(app, host='127.0.0.1', port=7262, log_config=None)
