41404d70d4f122df74796bfc9ed6668866717c26
CCXLV <abracadabragio12@gmail.com>
2024-11-17 22:43:20 +0400

    updated sync data

---
+++ app.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+from server import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=5101)
--- server/router/data_sync.py
+++ server/router/data_sync.py
@@ -1,14 +1,23 @@
 from fastapi import HTTPException
 from pydantic import BaseModel
 
+from googleapiclient.discovery import build
+
 class GoogleSheetsRequest(BaseModel):
-    token: str
+    api_key: str
+    spreadsheet_id: str
+    range_name: str
 
 class DataSync:
     async def get_google_sheets_data(request_data: GoogleSheetsRequest):
         try:
-            print(request_data.token)
+            service = build('sheets', 'v4', developerKey=request_data.api_key)
 
-            return {"success": "Data received"}
+            sheet = service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=request_data.spreadsheet_id, range=request_data.range_name).execute()
+        
+            values = result.get('values')
+
+            return {"data": values}
         except ValueError as e:
             raise HTTPException(status_code=400, detail="Bad Request") from e
